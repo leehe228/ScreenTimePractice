fb33841e12753b07a9f59938d87e876870a82918
Hoeun Lee <hoeunlee228@gmail.com>
2023-09-29 12:59:32 +0900

    Initial Commit

---
+++ ScreenTimePractice/ScreenTimePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimePracticeApp.swift
+//  ScreenTimePractice
+//
+//  Created by Hoeun Lee on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
